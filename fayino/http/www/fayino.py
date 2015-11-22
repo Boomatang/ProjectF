2958ebee4107eba6304f3eed8dedff750259f605
boomatang <jimfity@gmail.com>
2015-11-22 23:19:08 +0000

    More general Documents added. The biggest has to be the Site Map.

--- fayino/http/www/fayino.py
+++ fayino/http/www/fayino.py
@@ -3,9 +3,10 @@ app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home/')
 def hello_world():
-    return 'Hello World!'
+    return 'Hello World! This new'
 
 if __name__ == '__main__':
 
-    app.run(debug=True, port="6000")
+    app.run(port=7000, debug=True)
