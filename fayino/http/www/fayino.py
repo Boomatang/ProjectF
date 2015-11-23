e48738201c1f34bcb6c25ef7bab31694607aeafa
boomatang <jimfity@gmail.com>
2015-11-23 19:06:17 +0000

    Bootstrap added to project and the first few pages have been laid out for working on.

--- fayino/http/www/fayino.py
+++ fayino/http/www/fayino.py
@@ -1,11 +1,59 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+# ####################      Login Related Pages        ########################
+
+
+@app.route("/login/")
+def login():
+    return render_template("Login/login.html")
+
+
+# ####################      Sign Up Related Pages        ########################
+
+@app.route("/signUpCompleted/")
+def signUpComplated():
+    return render_template("SetUp/signUpCompleted.html")
+
+
+@app.route("/confirmUserDetails/")
+def confirmUserSetupDetails():
+    return render_template("SetUp/confirmDetails.html")
+
+
+@app.route('/companyDetails/')
+def setCompanyDetails():
+    return render_template("SetUp/setCompanyDetails.html")
+
+
+@app.route('/signup/')
+def signup():
+    return render_template("SetUp/signup.html")
+
+
+# ####################      Public Pages        ########################
 
 
 @app.route('/')
 @app.route('/home/')
-def hello_world():
-    return 'Hello World! This new'
+def publicHomePage():
+    return render_template("Public-html/index.html")
+
+
+# ####################      Error Pages        ########################
+# TODO set up some basic error handing pages
+
+@app.errorhandler(404)
+def fail404(e):
+    return render_template("Error/404.html")
+
+
+@app.errorhandler(405)
+def fail405(e):
+    return render_template("Error/405.html")
+
+
+# #######################################################################
 
 if __name__ == '__main__':
 
